c943861e00d19eb9ed0ee38c418cfa307af641af
Jesse Ashmore <jesse.ashmore97@gmail.com>
2022-09-17 18:48:13 +0100

    Change to pytest

--- test.py
+++ test.py
@@ -1,24 +1,44 @@
-from unittest import TestCase
+import pytest
 
-import cogs.notifications as notifications
 import cogs.text as text
+from cogs.notifications import Notifications
 from cogs.passive import try_match_youtube_video_for_spotify_track
+from main import bot, load_bot
 
 
-class Tests(TestCase):
+class Tests:
+    # bot = load_bot(bot)
+
     def test_partition_message(self):
         too_long = f"{'hello world'*100}\n" * 3
         text.partition_message(too_long)
         assert len(text.partition_message(too_long)) == 2
 
     def test_censor_phrases(self):
-        self.assertEqual(notifications.Notifications.censor_phrase("hello", True), "||hello||")
-        self.assertEqual(notifications.Notifications.censor_phrase("hello", False), "h||ell||o")
-        self.assertEqual(notifications.Notifications.censor_phrase("one shot", False), "||one|| s||ho||t")
+        assert Notifications.censor_phrase("hello", True) == "||hello||"
+        assert Notifications.censor_phrase("hello", False) == "h||ell||o"
+        assert Notifications.censor_phrase("one shot", False) == "||one|| s||ho||t"
 
     def test_spotify_grab_info(self):
-        self.assertIsNotNone(
+        assert (
             try_match_youtube_video_for_spotify_track(
                 "https://open.spotify.com/track/04mhI65E8cdqrhq6mh5IrJ?si=75d1be99334047ef"
             )
+            is not None
         )
+
+    # @pytest.mark.asyncio
+    # async def test_permissions(self):
+    #     notifications_: Notifications = self.bot.get_cog("notifications")
+    #     assert notifications_ is not None
+
+    #     test_user_bad = "113107906815094784"
+    #     test_user_good = "359409912893276161"
+    #     test_channel = "946167332303147049"
+
+    #     voice_channel = self.bot.get_channel(test_channel)
+    #     user_good = self.bot.get_user(test_user_good)
+    #     user_bad = self.bot.get_user(test_user_bad)
+
+    #     assert notifications_.user_can_connect_to_channel(user_good, voice_channel)
+    #     assert not notifications_.user_can_connect_to_channel(user_bad, voice_channel)
