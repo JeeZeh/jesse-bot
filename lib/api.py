736a22e9aa373b80bdb471b7755858b5b7452dc1
Jesse Ashmore <jesse.ashmore97@gmail.com>
2022-12-26 00:47:10 +0000

    refactor: bundle external API credentials into a single file

--- lib/api.py
+++ lib/api.py
@@ -4,17 +4,40 @@ from typing import Any
 import pyrebase
 from googleapiclient.discovery import build
 from spotipy import Spotify, SpotifyClientCredentials
+from .logger import logger
+
 
 firebase: Any = None
 spotify: Any = None
 youtube: Any = None
 
-with open("firebase.json") as fp:
-    firebase = pyrebase.initialize_app(json.load(fp))
+
+def init():
+    with open("service_credentials.json") as fp:
+        creds = json.load(fp)
+        init_spotify(creds["spotify"])
+        init_firebase(creds["firebase"])
+        init_youtube(creds["youtube"])
 
 
-with open("spotify.json") as fp:
-    creds = json.load(fp)
+def init_youtube(creds):
+    global youtube
+    logger.info("Initializing YouTube API")
+    api_service_name = "youtube"
+    api_version = "v3"
+    # Get credentials and create an API client
+    youtube = build(api_service_name, api_version, developerKey=creds["api_key"])
+
+
+def init_firebase(creds):
+    global firebase
+    logger.info("Initializing Firebase API")
+    firebase = pyrebase.initialize_app(creds)
+
+
+def init_spotify(creds):
+    global spotify
+    logger.info("Initializing Spotify API")
     spotify = Spotify(
         auth_manager=SpotifyClientCredentials(
             client_id=creds["client_id"],
@@ -22,9 +45,5 @@ with open("spotify.json") as fp:
         )
     )
 
-with open("youtube.json") as fp:
-    creds = json.load(fp)
-    api_service_name = "youtube"
-    api_version = "v3"
-    # Get credentials and create an API client
-    youtube = build(api_service_name, api_version, developerKey=creds["api_key"])
+
+init()
